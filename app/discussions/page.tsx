b181b8fa5cbbb0e76438a6f861704ede032164b1
dinujay10 <damsinijayatissa@gmail.com>
2025-01-08 23:44:25 +0530

    Fixed the HomePage video number and made the Discussion page

--- app/discussions/page.tsx
+++ app/discussions/page.tsx
@@ -1,11 +1,33 @@
+import VCard2 from "../Components/videoCards/vCard2";
+import VCard3 from "../Components/videoCards/vCard3";
+import VCard4 from "../Components/videoCards/vCard4";
+import VideoCardSet from "../Components/videoCards/VIdeoCardSet";
+import backgroundImage from "../public/images/article-bg.jpg";
+
+
 export default function Discussions() {
   return (
-    <div className="pt-20">
+    <div className="pt-32"
+    style={{
+      backgroundImage: `url(${backgroundImage.src})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    }}  
+    >
       <h1 className="text-center text-3xl font-bold">Discussions</h1>
       <p className="text-center mt-4">
         Learn more about Climate Advocates and our mission to create a better
         world.
       </p>
+          <div className="flex flex-col items-center w-12/12">
+            <div className=" gap-8  bg-opacity-70 h-1/2 rounded-3xl w-auto px-2 py-12 text-center flex flex-wrap justify-center">
+              <VideoCardSet />
+              <VCard2 />
+              <VCard3 />
+              <VCard4 />
+            </div>
+          </div>
+
     </div>
   );
 }
